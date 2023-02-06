149e94d7be0967d5f3177833103ec64ddbdb7774
Gashaw Kidane <kidanugashaw@gmail.com>
2023-02-06 22:52:59 +0300

    Dto applied for the app

--- Dtos/Character/GetCharacterDto.cs
+++ Dtos/Character/GetCharacterDto.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace dotnet_rpg.Models
+namespace dotnet_rpg.Dtos.Character
 {
-    public class Character
+    public class GetCharacterDto
     {
         public int Id { get; set; }
         public string Name { get; set; } = "Frodo";
-        public int Strength { get; set; }=100;
+        public int HitPoint { get; set; }=100;
+        public int Strength { get; set; }=10;
         public int Defense { get; set; }=10;
         public int Inteligence { get; set; }=10;
-        public RpgClass Class { get; set; }=RpgClass.Knight;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
     }
 }
--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_rpg.Dtos.Character;
 
 namespace dotnet_rpg.Services.CharacterService
 {
@@ -12,7 +13,7 @@ namespace dotnet_rpg.Services.CharacterService
             new Character{Id=1, Name="Gashaw"}
 
         };
-        public async Task<ServiceResponse< List<Character>>>AddCharacter(Character newCharacter)
+        public async Task<ServiceResponse< List<Character>>>AddCharacter(AddCharacterDto newCharacter)
         {
             var serviceResponse =new ServiceResponse<List<Character>> ();
             characters.Add(newCharacter);
@@ -20,14 +21,14 @@ namespace dotnet_rpg.Services.CharacterService
             return serviceResponse;
         }
 
-        public async Task<ServiceResponse< List<Character>>> GetAllCharacters()
+        public async Task<ServiceResponse< List<GetCharacterDto>>> GetAllCharacters()
         {
-             var serviceResponse =new ServiceResponse<List<Character>> ();
+             var serviceResponse =new ServiceResponse<List<GetCharacterDto>> ();
              serviceResponse.Data=characters;
             return serviceResponse;
         }
 
-        public async Task <ServiceResponse<Character>> GetCharacterById(int id)
+        public async Task <ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
         {
             var serviceResponse =new ServiceResponse<Character> ();
             var character=characters.FirstOrDefault(c => c.Id == id);
