31c323ded76aef623ccfc24fa565a5ed9d08d0f7
Gashaw Kidane <kidanugashaw@gmail.com>
2023-02-06 22:33:10 +0300

    Service folder created for more organizeds the file

---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        private static List<Character> characters = new List<Character>(){
+             new Character(),
+            new Character{Id=1, Name="Gashaw"}
+
+        };
+        public async Task<ServiceResponse< List<Character>>>AddCharacter(Character newCharacter)
+        {
+            var serviceResponse =new ServiceResponse<List<Character>> ();
+            characters.Add(newCharacter);
+            serviceResponse.Data=characters;
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse< List<Character>>> GetAllCharacters()
+        {
+             var serviceResponse =new ServiceResponse<List<Character>> ();
+             serviceResponse.Data=characters;
+            return serviceResponse;
+        }
+
+        public async Task <ServiceResponse<Character>> GetCharacterById(int id)
+        {
+            var serviceResponse =new ServiceResponse<Character> ();
+            var character=characters.FirstOrDefault(c => c.Id == id);
+             serviceResponse.Data=character;
+             return serviceResponse;
+        }
+           
+    }
+}
