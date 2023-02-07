c4ae651e752a8240e7a2dfb24fd1f9a9b47bd366
Gashaw Kidane <kidanugashaw@gmail.com>
2023-02-07 14:19:40 +0300

    Refactoring and the default constractor implemented

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,16 +1,23 @@
-global using Microsoft.EntityFrameworkCore;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 namespace dotnet_rpg.Data
 {
-    public class DataContext: DbContext
+    public class DataContext : DbContext
     {
-       public DataContext(DbContextOptions<DbContext> options ): base(options)
-       {
-        
-       } 
-       public DbSet<Character> Characters =>Set<Character>();
+        public DataContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+        public DataContext() : base()
+        {
+        }
+
+        public DbSet<Character> Characters => Set<Character>();
+
+
+       
     }
 }
