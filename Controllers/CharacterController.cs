828f0ee9c44c96a53bb3c0207a293be1418584bc
Gashaw Kidane <kidanugashaw@gmail.com>
2023-02-07 10:11:42 +0300

    NotFound is appropriate if the response is not found

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -43,8 +43,12 @@ namespace dotnet_rpg.Controllers
          [HttpPut]
         public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> UpdatedCharacter(UpdateCharacterDto updatedCharacter)
         {
+            var response=await _characterService.UpdateCharacter(updatedCharacter);
+            if(response.Data is null){
+                return NotFound(response);
+            }
 
-            return Ok(await _characterService.UpdateCharacter(updatedCharacter));
+            return Ok(response);
         }
     }
 }
