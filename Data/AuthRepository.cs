b66bf1572b2a65411829088a164c06644e5740da
Gashaw Kidane <kidanugashaw@gmail.com>
2023-02-09 11:21:45 +0300

    user registerd method implemented

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -7,19 +7,45 @@ namespace dotnet_rpg.Data
 {
     public class AuthRepository : IAuthRepository
     {
+        // To access the database we need the data context, so we add the constractor for this class and inject the data context
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+
+        }
         public Task<ServiceResponse<string>> Login(string username, string passwor)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ServiceResponse<int>> Register(User user, string passwor)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            CreatePasswordHash(password, out byte [] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash=passwordHash;
+            user.PasswordSalt=passwordSalt;
+            
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            var response = new ServiceResponse<int>();
+            response.Data = user.Id;
+            return response;
         }
 
         public Task<bool> UserExist(string username)
         {
             throw new NotImplementedException();
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            // with the help of the out keyword, we do not need to return anything(passwordHash and passwordSalt are avilable on register method )
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
