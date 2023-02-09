cec2f02493c5ad59148d2fdc4adebec40619efbc
Gashaw Kidane <kidanugashaw@gmail.com>
2023-02-09 11:27:05 +0300

    User existence checker method during registration  implemented

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -21,11 +21,11 @@ namespace dotnet_rpg.Data
 
         public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            CreatePasswordHash(password, out byte [] passwordHash, out byte[] passwordSalt);
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
 
-            user.PasswordHash=passwordHash;
-            user.PasswordSalt=passwordSalt;
-            
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             var response = new ServiceResponse<int>();
@@ -33,9 +33,15 @@ namespace dotnet_rpg.Data
             return response;
         }
 
-        public Task<bool> UserExist(string username)
+        public async Task<bool> UserExist(string username)
         {
-            throw new NotImplementedException();
+            if (await _context.Users.AnyAsync(u => u.UserName.ToLower() == username.ToLower()))
+            {
+                return true;
+            }
+            return false;
+
+
         }
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
